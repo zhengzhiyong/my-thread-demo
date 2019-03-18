5da37d242ef62a75becfd740c71c9182d5974dea
zhengzhiyong <zhengzhiyong@jumore.com>
2019-03-18 09:11:58 +0800

    ThreadPoolExecutor线程池参数说明

--- myThread/thread-demo/src/com/threadPool/demo/executer/ExecuterDemo.java
+++ myThread/thread-demo/src/com/threadPool/demo/executer/ExecuterDemo.java
@@ -31,6 +31,21 @@ public class ExecuterDemo {
 
     //无界队列的线程池的核心就在于coreSize，而maxSize不起作用
 
+    /**
+     *     public ThreadPoolExecutor(
+     *                               int corePoolSize,//线程池里的核心线程数量，初始化线程池的时候创建线程的数量
+     *                               int maximumPoolSize,//线程池里允许有的最大线程数量
+     *                               long keepAliveTime,//如果 当前线程数量 > corePoolSize，多出来的线程会在keepAliveTime之后就被释放掉
+     *                               TimeUnit unit,//keepAliveTime的时间单位,比如分钟,小时等
+     *                               BlockingQueue<Runnable> workQueue,//每当需要创建新的线程放入线程池的时候,就是通过这个线程工厂来创建的
+     *                               RejectedExecutionHandler handler//就是说当线程,队列都满了,之后采取的策略,比如抛出异常等策略
+     *                               ) {
+     *         this(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue,
+     *              Executors.defaultThreadFactory(), handler);
+     *     }
+     *
+     */
+
     static class ScheduledThreadPoolDemo{
         static class ExecutorThread implements Runnable{
             @Override
